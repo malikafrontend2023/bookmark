1378606a74098e11b2c601d18fb8dc336862b0dc
malikafrontend2023 <malikanasimova03@gmail.com>
2023-09-10 21:02:09 +0500

    html tree change BEM

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item');
-const elsTabsPanel = document.querySelectorAll('.tabs__panel');
+const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
 
 
@@ -11,7 +11,7 @@ function deactivateTavItems () {
 
 function deactivateTavPanels () {
   elsTabsPanel.forEach(function(elTabsPanel){
-    elTabsPanel.classList.remove('tabs__panel--active')
+    elTabsPanel.classList.remove('tabpanels__item--active')
   });
 }
 
@@ -27,14 +27,14 @@ elsTabLink.forEach(function(elTabLink){
     // Add active class to current tabs__item
     elTabLink.parentElement.classList.add('tabs__item--active')
 
-    //Remove active class form tabs__panel elements
+    //Remove active class form tabpanels__item elements
     deactivateTavPanels ();
 
     // Show active tab panel
     // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
     const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
 
-    elTargetPanel.classList.add('tabs__panel--active');
+    elTargetPanel.classList.add('tabpanels__item--active');
 
     console.log(elTabLink.dataset.tabTarget);
     })
